adc4bf5ef1bf045e7deb0fe12f6f428f97f5c9cb
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-20 14:29:17 -0500

    Created Express server and set up HTML routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,5 +3,30 @@
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-// need to require friends module exports here
-// will do logic to compare friends here (inside of post route)
+// ===============================================================================
+// LOAD DATA
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  // API POST Requests
+
+  app.post("/api/friends", function(req, res) {
+    // will do logic to compare friends here (inside of post route)
+  });
+};
--- server.js
+++ server.js
@@ -11,3 +11,10 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
