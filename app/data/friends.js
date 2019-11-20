0b9a24f990893ae53eb0c85ae6c62adc8b2ee996
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-20 14:07:58 -0500

    Created front-end HTML files

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "https://picresize.com/images/rsz_lagotto-romangolo-tongue-out.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Bernie",
+    photo: "https://picresize.com/images/rsz_51.jpg",
+    scores: [2, 2, 4, 3, 3, 3, 4, 5, 3, 3]
+  },
+  {
+    name: "Charlie",
+    photo: "https://picresize.com/images/rsz_9.jpg",
+    scores: [4, 3, 3, 5, 1, 4, 1, 5, 4, 1]
+  },
+  {
+    name: "Danielle",
+    photo: "https://picresize.com/images/rsz_7.jpg",
+    scores: [2, 1, 1, 5, 3, 5, 2, 1, 1, 5]
+  },
+  {
+    name: "Eugene",
+    photo: "https://picresize.com/images/rsz_6.jpg",
+    scores: [2, 2, 2, 1, 2, 4, 3, 3, 5, 3]
+  },
+  {
+    name: "Fred",
+    photo: "https://picresize.com/images/rsz_5.jpg",
+    scores: [1, 5, 2, 1, 3, 5, 4, 3, 4, 3]
+  },
+  {
+    name: "Georgia",
+    photo: "https://picresize.com/images/rsz_4.jpg",
+    scores: [5, 1, 4, 5, 4, 4, 5, 1, 4, 2]
+  },
+  {
+    name: "Harold",
+    photo: "https://picresize.com/images/rsz_3.jpg",
+    scores: [4, 1, 5, 3, 1, 2, 1, 3, 5, 2]
+  },
+  {
+    name: "Ingrid",
+    photo: "https://picresize.com/images/rsz_22.jpg",
+    scores: [3, 3, 3, 4, 3, 4, 3, 1, 4, 2]
+  },
+  {
+    name: "Jasmine",
+    photo: "https://picresize.com/images/rsz_8.jpg",
+    scores: [4, 2, 4, 2, 2, 5, 5, 3, 1, 5]
+  }
+];
+
+module.exports = friendsArray;
